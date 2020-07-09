268fd3747b2348ba20268a3d5191e4b7afbb13ad
hshan <hhsung0120@naver.com>
2020-07-09 23:03:16 +0900

    뿌듯하고 재밌는 공부.. oauth2 구글 로그인까지 구현 완료 스프링부트와 aws로 혼자 구현하는 웹 서비스 이동욱 지움

--- src/main/java/com/heeseong/book/springboot/web/IndexController.java
+++ src/main/java/com/heeseong/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.heeseong.book.springboot.web;
 
+import com.heeseong.book.springboot.config.auth.dto.SessionUser;
 import com.heeseong.book.springboot.service.PostsService;
 import com.heeseong.book.springboot.web.dto.HelloResponseDto;
 import com.heeseong.book.springboot.web.dto.PostsResponseDto;
@@ -9,16 +10,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @Autowired
-    private PostsService postsService;
+    private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
