0367c26b626d15eaac008d36eae09b7f4b39aeda
hshan <hhsung0120@naver.com>
2020-07-19 21:34:48 +0900

    test

--- src/test/java/com/heeseong/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/heeseong/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class PostsRepositoryTest {
 
     @Autowired
